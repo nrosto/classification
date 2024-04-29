ebc0e217e70c22f1350cd3947ecb88651fb649dc
nrosto <nrotolamak@gmail.com>
2024-04-29 11:02:31 +0300

    Добавил предикт модели
    - Добавил предикт изображения для модели

--- frontend/src/components/upload.tsx
+++ frontend/src/components/upload.tsx
@@ -1,13 +1,33 @@
 'use client'
 import { useState } from 'react';
+import * as tf from '@tensorflow/tfjs';
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const IMG_SIZE = 200;
 
   const handleImageUpload = (event) => {
     const imageFile = event.target.files[0];
     setSelectedImage(URL.createObjectURL(imageFile));
   };
+
+  const classifyImage = async () => {
+    const imageElement = document.createElement('img');
+    imageElement.src = selectedImage;
+    await tf.ready();
+    const model = await tf.loadGraphModel('http://127.0.0.1:8080/model.json');
+    // const model = await tf.loadGraphModel('https://raw.githubusercontent.com/nrosto/classification/main/frontend/model/model_2_classes/model.json');
+    const result = await model.predict(tf.browser.fromPixels(imageElement).resizeNearestNeighbor([IMG_SIZE, IMG_SIZE]).toFloat().div(tf.scalar(255)).expandDims());
+    const predictions = Array.from(result.dataSync());
+    
+    // Определяем вероятности для каждого класса
+    const probabilityClass1 = predictions[0];
+    const probabilityClass2 = predictions[1];
+
+    // Устанавливаем вероятности в состояние компонента
+    setPrediction({ class1: probabilityClass1, class2: probabilityClass2 });
+};
 
   return (
     <div className="container">
@@ -26,8 +46,15 @@ export default function Home() {
       </div>
       <div className="textColumn">
         <h2>Проверка</h2>
-        <p>Привет мир!</p>
+        <button onClick={classifyImage}>Классифицировать</button>
+        {prediction && (
+            <div className='Prediction'>
+                <h2>Результат классификации:</h2>
+                <p>Вероятность Class 1: {prediction.class1}</p>
+                <p>Вероятность Class 2: {prediction.class2}</p>
+            </div>
+        )}
       </div>
     </div>
   );
-} 
+}
